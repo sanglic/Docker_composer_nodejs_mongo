22327752b8d67300b81b4fc85e19d858eb056d5e
sanglic <josepengineer@gmail.com>
2020-07-09 14:29:08 +0200

    docker

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/PruebaNode', {
+mongoose.connect('mongodb://mongo/PruebaNode', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
